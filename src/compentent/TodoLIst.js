de9f0164dbf321604527cc9727e2edf9747d862d
Mohamed Messoud <109314586+MohemedMSD@users.noreply.github.com>
2022-10-04 19:03:21 +0100

    Update TodoLIst.js

--- src/compentent/TodoLIst.js
+++ src/compentent/TodoLIst.js
@@ -11,7 +11,7 @@ function TodoLIst({tasks, funcdelet, SetComplet, EditTask}) {
                 // Maping in Array Task 
                 tasks && tasks.map((task, key) =>
                     <div key={key} data-key={task.id} className='div_task col-12 col-md-6 col-lg-4 col-xl-3 '>
-                        <div  className={task.complete ? 'task rounded completed d-flex justify-content-between p-2 mt-3' : 'task rounded d-flex justify-content-between p-2 mt-3'}>
+                        <div  className={`${task.complete ? 'completed' : ''} task rounded d-flex justify-content-between p-2 mt-3`}>
                             <p onClick={() => SetComplet(task.id)} className="col-7 my-auto ps-2">
                                 {task.task}
                                 {
@@ -20,8 +20,8 @@ function TodoLIst({tasks, funcdelet, SetComplet, EditTask}) {
                             </p>
                             
                             <div className='icons col-4 p-0 d-flex align-items-center justify-content-around'>
-                                <FiEdit className='icon' size={20} onClick={()=> EditTask(task.id)}/>
-                                <AiTwotoneDelete className='icon' size={24} onClick={(e)=> funcdelet(e, task.id)}/>
+                                <FiEdit className='icon edit' size={20} onClick={()=> EditTask(task.id)}/>
+                                <AiTwotoneDelete className='icon delet' size={24} onClick={(e)=> funcdelet(e, task.id)}/>
                             </div>
                         </div>
                     </div>
